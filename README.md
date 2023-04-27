78b4b6d67cf7d095a46ead6fcf5efc2d2e760b69
Matthew W Garvey <mwgarveyaa@gmail.com>
2023-04-27 12:41:24 -0400

    Oops. Forgot 60% of the Project. Fixed.

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ To Run, do this from the root:
 ```
 s/run
 ```
+To see via Render click here:
+
+[Matthew Garvey Software Development Tools Sprint Project](https://matthew-garvey-sdt-sprint-project.onrender.com)
+
 
 ## Technologies Used:
 - Version Control
@@ -24,20 +28,28 @@ s/run
         - processing data
     - plotly_express
         - making charts
+- Deployment
+    - Render
 
 ## To Do:
+### Data Preprocessing
+- [x] Handle Missing Values
+- [x] Handle Duplicate Data
+
 ### Code
 - [x] Get prototpye running
 - [x] Make one or more custom histograms
 - [x] Make one or more custom scatter plots
 - [x] Add Checkbox that changes one or more of the graphs in some way
+
 ### Deploy
 - [x] Make streamlit config file
 - [x] Make new web service on Render
 - [x] Link to GitHub
 - [x] Configure on Render
 - [x] Deploy on Render
+
 ### Submit
-- [ ] Final check to make sure it works
-- [ ] Update Readme
-- [ ] Submit
+- [x] Final check to make sure it works
+- [x] Update Readme
+- [x] Submit
--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as sl
 import pandas as pd
 import plotly_express as px
 
-df = pd.read_csv('./data/vehicles_us.csv')
-df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+df = pd.read_csv('./data/processed_vehicles_us.csv')
+# df['make'] = df['model'].apply(lambda x: x.split()[0])
 
 # Convert date column to datetime type
 df['date'] = pd.to_datetime(df['date_posted'])
@@ -12,30 +12,30 @@ df['day_of_week'] = df['date'].dt.day_name()
 
 filtered_df = df.copy()
 
-manufacturers = sorted(df['manufacturer'].unique().tolist())
+makes = sorted(df['make'].unique().tolist())
 
-for i, m in enumerate(manufacturers):
-    manufacturers[i] = m
+for i, m in enumerate(makes):
+    makes[i] = m
 
-sl.sidebar.markdown("## Show these Manufacturers \n(if none are selected, All Manufacturers will show)")
+sl.sidebar.markdown("## Show these makes \n(if none are selected, All makes will show)")
 
-# Add checkboxes to the sidebar for each manufacturer
+# Add checkboxes to the sidebar for each make
 checkboxes = []
-for m in manufacturers:
+for m in makes:
     checkbox = sl.sidebar.checkbox(f'Show {m.upper()} cars')
     checkboxes.append(checkbox)
 
 # Create a function that filters the data based on selected values
-def filter_data_by_manufacturer(df, checkboxes):
+def filter_data_by_make(df, checkboxes):
     filtered_df = df.copy()
-    selected_manufacturers = []
+    selected_makes = []
 
     for i, checkbox in enumerate(checkboxes):
         if checkbox:
-            selected_manufacturers.append(manufacturers[i])
+            selected_makes.append(makes[i])
     
-    if selected_manufacturers:
-        filtered_df = filtered_df[filtered_df['manufacturer'].isin(selected_manufacturers)]
+    if selected_makes:
+        filtered_df = filtered_df[filtered_df['make'].isin(selected_makes)]
     
     return filtered_df
 
@@ -47,7 +47,7 @@ selected_conditions = sl.sidebar.multiselect('Filter by one or more conditions:'
 
 # Create a function that filters the data based on selected values
 def filter_data_by_condition(df, column, checkboxes, selected_values):
-    filtered_df = filter_data_by_manufacturer(df, checkboxes)
+    filtered_df = filter_data_by_make(df, checkboxes)
     if selected_values:
         filtered_df = filtered_df.loc[filtered_df[column].isin(selected_values)]
     
@@ -58,10 +58,10 @@ sl.sidebar.markdown("## Show these Graphs")
 # Create checkboxes to allow the user to choose which charts to display
 show_hg = sl.sidebar.checkbox('Show Histogram of Distribution of Car Prices by Condition', True)
 show_sp = sl.sidebar.checkbox('Show Scatter Plot of Price vs. Mileage by Contition', True)
-show_lc = sl.sidebar.checkbox('Show Line Chart of Average Car Price Over Time by Condition', False)
+show_lc = sl.sidebar.checkbox('Show Line Chart of Average Car Price by Model Year by Condition', False)
 show_bc = sl.sidebar.checkbox('Show Bar Chart of Count of Cars by Fuel Type and Condition', False)
-show_bp = sl.sidebar.checkbox('Show Box and Whisker Plot of Price by Car Manufacturer and Condition', False)
-show_hm = sl.sidebar.checkbox('Show Heatmap of Average Price by Fuel and Transmission', False)
+show_bp = sl.sidebar.checkbox('Show Box and Whisker Plot of Price by Car make and Condition', False)
+show_hm = sl.sidebar.checkbox('Show Heatmap of Average Price by Fuel Type and Transmission', False)
 show_tm = sl.sidebar.checkbox('Show Treemap of Count of Cars by Make and Model', False)
 show_pc = sl.sidebar.checkbox('Show Polar Chart of Average Car Price by Day of Week and Condition', False)
 show_3dsp = sl.sidebar.checkbox('Show 3D Surface Plot of Price vs. Odometer, Model Year, and Condition', False)
@@ -85,26 +85,26 @@ if sl.sidebar.button('Render'):
     # to show or hide them to the sidebar)
     if show_lc:
         # A Line Chart
-        fig_lc = px.line(filtered_df, x='model_year', y='price', title='Line Chart of Average Car Price Over Time by Condition', color='condition')
+        fig_lc = px.line(filtered_df, x='year', y='price', title='Line Chart of Average Car Price by Model Year by Condition', color='condition')
         sl.write(fig_lc)
     if show_bc:
         # A bar chart
-        fig_bc = px.bar(filtered_df, x='fuel', title='Bar Chart of Count of Cars by Fuel Type and Condition', color='condition')
+        fig_bc = px.bar(filtered_df, x='fuel_type', title='Bar Chart of Count of Cars by fuel Type and Condition', color='condition')
         sl.write(fig_bc)
     if show_bp:
         # A box plot
-        fig_bp = px.box(filtered_df, x='manufacturer', y='price', title='Box and Whisker Plot of Price by Car Manufacturer and Condition', color='condition')
+        fig_bp = px.box(filtered_df, x='make', y='price', title='Box and Whisker Plot of Price by Car make and Condition', color='condition')
         sl.write(fig_bp)
     if show_hm:
-        # Calculate the average price by fuel and transmission
-        df_agg = df.pivot_table(index='fuel', columns='transmission', values='price', aggfunc='mean')
+        # Calculate the average price by fuel_type and transmission
+        df_agg = df.pivot_table(index='fuel_type', columns='transmission', values='price', aggfunc='mean')
 
         # Create a heatmap
-        fig_hm = px.imshow(df_agg, x=df_agg.columns, y=df_agg.index, color_continuous_scale='Blues', title='Heatmap of Average Price by Fuel and Transmission')
+        fig_hm = px.imshow(df_agg, x=df_agg.columns, y=df_agg.index, color_continuous_scale='Blues', title='Heatmap of Average Price by Fuel Type and Transmission')
         sl.write(fig_hm)
     if show_tm:
         # A treemap
-        fig_tm = px.treemap(filtered_df, path=['manufacturer', 'model'], title='Treemap of Count of Cars by Make and Model')
+        fig_tm = px.treemap(filtered_df, path=['make', 'model'], title='Treemap of Count of Cars by Make and Model')
         sl.write(fig_tm)
     if show_pc:
         # A polar chart
@@ -112,7 +112,7 @@ if sl.sidebar.button('Render'):
         sl.write(fig_pc)
     if show_3dsp:
         # A 3D surface plot
-        fig_3dsp = px.scatter_3d(filtered_df, x='odometer', y='model_year', z='price', title='3D Surface Plot of Price vs. Odometer, Model Year, and Condition', color='condition')
+        fig_3dsp = px.scatter_3d(filtered_df, x='odometer', y='year', z='price', title='3D Surface Plot of Price vs. Odometer, Model Year, and Condition', color='condition')
         sl.write(fig_3dsp)
 else:
     sl.write('Click the "Render" button to display the data and charts.')
